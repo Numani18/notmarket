fcfa8aa92d4932ca876461e01b170745715806e6
YAMAN ATAOĞLU <yamanataoglu@MacBook-Air.local>
2026-07-23 12:52:07 +0300

    Service worker kaldırıldı (production'da CSS'i bozuyordu), mevcut SW'ler temizleniyor

--- components/ServiceWorker.tsx
+++ components/ServiceWorker.tsx
@@ -2,11 +2,17 @@
 
 import { useEffect } from 'react'
 
-// Service worker'ı kaydeder (PWA kurulabilirliği için)
+// Önceden kaydedilmiş service worker'ları temizler.
+// (Eski SW production'da CSS yüklenmesini bozuyordu; PWA/TWA için SW şart değil.)
 export default function ServiceWorker() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js').catch(() => {})
+      navigator.serviceWorker.getRegistrations().then((regs) => {
+        regs.forEach((r) => r.unregister())
+      }).catch(() => {})
+      if (window.caches) {
+        caches.keys().then((keys) => keys.forEach((k) => caches.delete(k))).catch(() => {})
+      }
     }
   }, [])
   return null
